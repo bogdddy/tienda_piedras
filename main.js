55622f4e5b57a0419e167ac6e24ed88d5fabb763
bogdan <bogdan.struk19@gmail.com>
2021-05-28 18:51:45 +0200

    añadir elementos en tabla

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ function allowDrop(ev) {
 }
   
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("ore", ev.target.id);
 }
   
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    document.getElementById("carrito").insertRow(-1)
+    var data = ev.dataTransfer.getData("ore");
+    myCreateFunction(data);
 }
+
+function myCreateFunction(ore) {
+    var table = document.getElementById("carrito");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = ore;
+    cell2.innerHTML = '<input type="number" value="1"></input>';
+  }
