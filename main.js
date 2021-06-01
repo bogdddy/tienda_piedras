720246a07fd4e967ff5ddb663eb7de09030d001d
aleix <al3ixgh@gmail.com>
2021-06-01 18:25:47 +0200

    añadida funcion para calcular precio

--- main.js
+++ main.js
@@ -13,19 +13,19 @@ function drop(ev) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData("ore");
     checkProduct(data);
-    
+
 }
 
 
- var lista=[];       //Lista de productos   
-var cantidad=[];    //Cantidad de cada producto
+var lista = [];       //Lista de productos   
+var cantidad = [];    //Cantidad de cada producto
 
 //Comprueba si el producto ya se ha seleccionado anteriormente
-function checkProduct(data){
-    if(lista.indexOf(data)==-1){
+function checkProduct(data) {
+    if (lista.indexOf(data) == -1) {
         myCreateFunction(data);
     }
-    else{
+    else {
         addItem(data);
     }
 }
@@ -39,28 +39,67 @@ function myCreateFunction(ore) {
     lista.push(ore)
     cantidad.push(1);
     cell1.innerHTML = ore;
-    cell2.innerHTML = '<input type="number" id=input_'+ore+' value="1"></input>';
+    cell2.innerHTML = '<input type="number" id=input_' + ore + ' value="1"></input>';
 }
 
 //Si el producto ya se ha seleccionado anteriormente, suma 1 al valor de su input
-  function addItem(ore){
-    let input = document.getElementById("input_"+ore);
-    let aux=parseInt(input.value);
-    input.value=++aux;
-    cantidad[lista.indexOf(ore)]+=1;
+function addItem(ore) {
+    let input = document.getElementById("input_" + ore);
+    let aux = parseInt(input.value);
+    input.value = ++aux;
+    cantidad[lista.indexOf(ore)] += 1;
     console.log(cantidad);
-  }
+}
 
-  console.log(lista);
-  console.log(cantidad);
+console.log(lista);
+console.log(cantidad);
 //   var prueba=document.getElementById("add_Iron");
 //   prueba.addEventListener("click", checkProduct("Iron_Ore"));
-document.getElementById("add_Iron").addEventListener("click", function(){checkProduct("Iron_Ore");})
-document.getElementById("add_Gold").addEventListener("click", function(){checkProduct("Gold_Ore");})
-document.getElementById("add_Emerald").addEventListener("click", function(){checkProduct("Emerald_Ore");})
-document.getElementById("add_Lapis_Lazuli").addEventListener("click", function(){checkProduct("Lapis_Lazuli_Ore");})
-document.getElementById("add_Diamond").addEventListener("click", function(){checkProduct("Diamond_Ore");})
-document.getElementById("add_Nether_Quartz").addEventListener("click", function(){checkProduct("Nether_Quartz_Ore");})
+document.getElementById("add_Iron").addEventListener("click", function () { checkProduct("Iron_Ore"); });
+document.getElementById("add_Gold").addEventListener("click", function () { checkProduct("Gold_Ore"); });
+document.getElementById("add_Emerald").addEventListener("click", function () { checkProduct("Emerald_Ore"); });
+document.getElementById("add_Lapis_Lazuli").addEventListener("click", function () { checkProduct("Lapis_Lazuli_Ore"); });
+document.getElementById("add_Diamond").addEventListener("click", function () { checkProduct("Diamond_Ore"); });
+document.getElementById("add_Nether_Quartz").addEventListener("click", function () { checkProduct("Nether_Quartz_Ore"); });
+//boton finalizar compra
+document.getElementById("checkout").addEventListener("click", function () { alert(totalPrice()); });
 
+function totalPrice() {
+    let price = 0;
 
+    lista.forEach(ore => {
+
+        switch (ore) {
+            case "Iron_Ore":
+                price = price + (20 * cantidad[lista.indexOf(ore)]);
+                break;
+
+            case "Gold_Ore":
+                price = price + (50 * cantidad[lista.indexOf(ore)]);
+                break;
+
+            case "Emerald_Ore":
+                price = price + (100 * cantidad[lista.indexOf(ore)]);
+                break;
+
+            case "Lapis_Lazuli_Ore":
+                price = price + (80 * cantidad[lista.indexOf(ore)]);
+                break;
+
+            case "Diamond_Ore":
+                price = price + (1000 * cantidad[lista.indexOf(ore)]);
+                break;
+
+            case "Nether_Quartz_Ore":
+                price = price + (500 * cantidad[lista.indexOf(ore)]);
+                break;
+
+        }
+    });
+    let table = document.getElementById("carrito");
+    table.innerHTML="<tr><th>Producto</th><th>Cantidad</th></tr>";
+    lista=[];
+    cantidad=[];
+    return(price);
+}
 
